fa2f737331ea28a1d8485f0d9e828351e076f234
lishaohui <123>
2019-03-21 11:24:24 +0800

    更正相应平台驱动文件夹名称

--- driver_arm_aarch64/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:./gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/
-make main CROSS_COMPILE=aarch64-linux-gnu-
-sync
--- driver_arm_aarch64/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
-./main  $1 $2 $3 $4
-
